e93c212dadacb7445214533a038956240e5dec41
Joao Araujo <joaosantos162003@gmail.com>
2024-12-16 13:59:41 -0300

    Adicionando rota de usuários e ajustando importações no servidor

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-//Importação de frameworks e funções no projeto, uso de dotenv para o auth.
-require('dotenv').config();
-const express = require('express')
-const {urlNotValid} = require('./middlewares/auth.js')
-const characters = require('./routes/characters')
-const starships = require('./routes/starships')
-const planets = require('./routes/planets')
-const swaggerDoc = require('./swagger-output.json');
-const swaggerUi = require('swagger-ui-express')
+require("dotenv").config();
+const express = require("express");
+const { urlNotValid } = require("./middlewares/auth.js");
+const characters = require("./routes/characters");
+const users = require("./routes/users.js");
+const planets = require("./routes/planets");
+const swaggerDoc = require("./swagger-output.json");
+const swaggerUi = require("swagger-ui-express");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-//O servidor rodará na porta 3000, tendo 3 rotas principais, /users, /countries, /cities.
-app.use('/characters', characters)
-app.use('/starships', starships)
-app.use('/planets', planets)
+app.use("/characters", characters);
+app.use("/users", users);
+app.use("/planets", planets);
 
-//Rota como pedido nos requisitos para visualizar a documentação da API gerada automaticamente pelo swagger
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
+app.use(urlNotValid);
 
-//Chamada de middleware do arquivo auth.js para caso não encontre a rota
-app.use(urlNotValid)
-
-app.listen(3000, () => console.log("Servidor rodando na porta 3000"))
+app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
--- swagger.js
+++ swagger.js
@@ -1,15 +1,13 @@
-const swaggerAutogen = require('swagger-autogen')();
+const swaggerAutogen = require("swagger-autogen")();
 
-//Criará um arquivo chamado swaggert-output com a documentação
-const outputFile = './swagger-output.json';
+const outputFile = "./swagger-output.json";
 
-//Inserção dos arquivos que contém as rotas
-const endpointsFiles = ['./routes/characters.js', './routes/starships.js', './routes/planets.js'];
-
-//Gera a documentação automaticamente ao rodar o index.js
-//Desativei ele abaixo pelo fato de já ter gerado uma vez
-//Se rodar novamente ele irá "reiniciar" o swagger-output e apagar os ajustes personalizados
+const endpointsFiles = [
+  "./routes/characters.js",
+  "./routes/users.js",
+  "./routes/planets.js",
+];
 
 swaggerAutogen(outputFile, endpointsFiles).then(() => {
-  require('./index.js');
+  require("./index.js");
 });
