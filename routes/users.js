5788676eb65ab347fd0bd8b6055b45b1f9b3256d
Joao Araujo <joaosantos162003@gmail.com>
2024-12-17 16:41:22 -0300

    Co-Authored by Victor Matheus refactor: update user routes and middleware for improved authorization

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,8 @@
 const express = require("express");
 
-const usersControler = require("../controllers/usersController");
+const usersControler = require("../controllers/users.js");
 
-const {
-  verifyToken,
-  isAdm,
-  userIsAdmOrHimself,
-} = require("../middlewares/auth.js");
+const { verifyToken, typeOfUser } = require("../middleware/auth.js");
 const router = express.Router();
 
 router.get("/install", usersControler.createInitialAdm);
@@ -15,17 +11,11 @@ router.get("/", usersControler.getUsers);
 router.get("/:id", usersControler.getUserById);
 
 router.post("/registerUser", usersControler.createUser);
-router.post("/registerAdm", verifyToken, isAdm, usersControler.createUserAdm);
+router.post("/registerAdm", verifyToken, typeOfUser, usersControler.createUserAdm);
 router.post("/login", usersControler.verifyUser);
 
-router.put(
-  "/updateUser",
-  verifyToken,
-  userIsAdmOrHimself,
-  usersControler.updateUser
-);
-router.put("/updateAdm", verifyToken, isAdm, usersControler.updateUserAdm);
-
-router.delete("/:id", verifyToken, isAdm, usersControler.deleteUser);
+router.put("/updateUser", verifyToken, typeOfUser, usersControler.updateUser);
+router.put("/updateAdm", verifyToken, typeOfUser, usersControler.updateUserAdm);
+router.delete("/:id", verifyToken, typeOfUser, usersControler.deleteUser);
 
 module.exports = router;
